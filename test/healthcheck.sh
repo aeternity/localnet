169512cc1d79e034cd8d7219b6928eea4f05aa2b
Dincho Todorov <dincho@users.noreply.github.com>
2019-08-07 17:22:38 +0300

    Initial commit/sync from aeternity repo

---
+++ test/healthcheck.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# As this script might be used as docker health check it should exit with either 0/1
+
+EXTERNAL_ADDRESS=${EXTERNAL_ADDRESS:-localhost:3013}
+INTERNAL_ADDRESS=${INTERNAL_ADDRESS:-localhost:3113}
+MIN_PEERS=${MIN_PEERS:-2}
+
+# External API
+curl -s -f -S -o /dev/null --retry 6 --retry-connrefused http://${EXTERNAL_ADDRESS}/v2/status || exit 1
+
+# Internal API
+PEERS_COUNT=$(curl -s -S ${INTERNAL_ADDRESS}/v2/debug/peers | grep -o aenode | wc -l)
+
+# Explicit exit because otherwise test would exit with status 127
+test $PEERS_COUNT -ge $MIN_PEERS || exit 1
