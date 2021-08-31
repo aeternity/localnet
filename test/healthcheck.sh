fcb4794b7fdc0901d3a39162309d1d7b0f1d8d85
Dincho Todorov <dincho@users.noreply.github.com>
2021-08-31 17:32:53 +0300

    Refactor to default single node

--- test/healthcheck.sh
+++ test/healthcheck.sh
@@ -16,10 +16,14 @@ PEERS_COUNT=$(curl -sS ${INTERNAL_ADDR}/v2/debug/peers | grep -o aenode | wc -l)
 echo "Testing internal API: $PEERS_COUNT peers"
 test $PEERS_COUNT -ge $MIN_PEERS || exit 3
 
-echo "Testing WebSockets API: ${WEBSOCKET_ADDR}"
-WS_STATUS=$(curl -sS -o /dev/null --retry 6 \
-    -w "%{http_code}" \
-    http://${WEBSOCKET_ADDR}/channel)
-# The proxy handles connection upgrade, and we send bad request anyway.
-# It shouldn't be 404 so we're good.
-test $WS_STATUS -ne 404
+# Disable State Channels websocket test as the node crash with 500 and curl retry on 500
+# This takes ~1 minute to complete this test - each time
+# https://github.com/aeternity/aeternity/issues/3131
+
+# echo "Testing WebSockets API: ${WEBSOCKET_ADDR}"
+# WS_STATUS=$(curl -sS -o /dev/null --retry 6 \
+#     -w "%{http_code}" \
+#     http://${WEBSOCKET_ADDR}/channel)
+# # The proxy handles connection upgrade, and we send bad request anyway.
+# # It shouldn't be 404 so we're good.
+# test $WS_STATUS -ne 404
